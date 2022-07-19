ef683a508c2ba2d38e7171fcc6c56936cd49d0a3
Stefan Koch <stefan.koch@micromeritics.com>
2022-07-19 09:37:14 -0400

    Bug: MettlerToledo mass balance did not send mass to entry field.
    Root Cause:  The mass balance sent keyboard sequence over the usb but this was interpreted
    as control charaters.  (It worked in windows with excel/notepad.) That is because the
    numlock was not on.
    
    Solution: Add the numlockx package to the basesystem.  Then call this to set the numlock
    to on as part of the mic-chromium service.  This is a good spot to load it since it is
    here that the window server is running, and it is not needed except inside chromium.
    
    Why and When Introduced:  Linux just disables this by default on startup.
    
    Possible Similar problems: Where Looked, What Found, and What Action: NA
    
    Testing: Made a clean basesystem build and verified that the MettlerToledo system will
    work.  Also verified that the Sartorious balance still works.

--- recipes-local/micro/files/mic-chromium.sh
+++ recipes-local/micro/files/mic-chromium.sh
@@ -22,7 +22,13 @@ else
     TOUCH_PARAM="--touch-devices=$TOUCH_DEVICE_ID --touch-events"
 fi
 
+# run chromium
 chromium --kiosk --no-sandbox --start-fullscreen --fast --fast-start --disable-infobars --password-store=basic \
          --app=http://localhost:80 --top-chrome-touch-ui $TOUCH_PARAM \
          --enable-features=OverlayScrollbar &
 sleep 5
+
+# make sure that the num-lock is pressed.  This ensures that the values of the
+# MettlerTolelo mass balance are properly interpreted.  For some reason this
+# must be run after chromium is started, otherwise it does not work.
+numlockx
